bae4abd318369b37389324b69ec8f758d2b864ea
Artem Prigoda <artem.prigoda@glispamedia.com>
2016-10-03 21:35:57 +0200

    Add support for naming strategies

--- src/main/java/com/github/arteam/jdbi3/JdbiFactory.java
+++ src/main/java/com/github/arteam/jdbi3/JdbiFactory.java
@@ -1,10 +1,14 @@
 package com.github.arteam.jdbi3;
 
+import com.github.arteam.jdbi3.strategies.DelegatingStatementNameStrategy;
+import com.github.arteam.jdbi3.strategies.NameStrategies;
 import io.dropwizard.db.ManagedDataSource;
 import io.dropwizard.db.PooledDataSourceFactory;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.util.Duration;
 import org.jdbi.v3.core.Jdbi;
+
+import static com.codahale.metrics.MetricRegistry.name;
 
 /**
  * @author Artem Prigoda
@@ -12,12 +16,23 @@ import org.jdbi.v3.core.Jdbi;
  */
 public class JdbiFactory {
 
+    private static class SanerNamingStrategy extends DelegatingStatementNameStrategy {
+        private SanerNamingStrategy() {
+            super(NameStrategies.CHECK_EMPTY,
+                    NameStrategies.CONTEXT_CLASS,
+                    NameStrategies.CONTEXT_NAME,
+                    NameStrategies.SQL_OBJECT,
+                    statementContext -> name(Jdbi.class, "raw-sql"));
+        }
+    }
+
     public Jdbi build(Environment environment,
                       PooledDataSourceFactory configuration,
                       String name) {
         ManagedDataSource dataSource = configuration.build(environment.metrics(), name);
         String validationQuery = configuration.getValidationQuery();
         Jdbi jdbi = Jdbi.create(dataSource);
+        jdbi.setTimingCollector(new InstrumentedTimingCollector(environment.metrics(), new SanerNamingStrategy()));
         jdbi.installPlugins();
 
         environment.lifecycle().manage(dataSource);
@@ -25,6 +40,7 @@ public class JdbiFactory {
                 environment.getHealthCheckExecutorService(),
                 configuration.getValidationQueryTimeout().orElseGet(() -> Duration.seconds(5)),
                 jdbi, validationQuery));
+
         return jdbi;
     }
 }
